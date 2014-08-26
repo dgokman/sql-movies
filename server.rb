6171ff9867905730c0bf9ca666ffdbf229759a2a
Dennis Gokman <dennis.gokman@gmail.com>
2014-08-26 17:24:35 -0400

    added movie links

--- server.rb
+++ server.rb
@@ -21,7 +21,7 @@ get '/actors' do
   end
 
   erb :'actors/index'
-  #binding.pry
+
 end
 
 get '/actors/:id' do
@@ -35,7 +35,7 @@ get '/actors/:id' do
       JOIN actors ON actors.id = cast_members.actor_id
       WHERE actors.id = $1', [@actor_id])
     @actors_id = results.to_a
-    #binding.pry
+
   end
   erb :'actors/show'
 end
@@ -49,7 +49,7 @@ get '/movies' do
       JOIN studios ON movies.studio_id = studios.id
       ORDER BY movie')
     @movies = results.to_a
-    #binding.pry
+
   end
   erb :'movies/index'
 end
@@ -57,7 +57,7 @@ end
 get '/movies/:id' do
   @movie_id = params[:id]
   db_connection do |conn|
-    results = conn.exec('SELECT movies.id AS id, movies.title AS movie, movies.year AS year, actors.name AS actor,
+    results = conn.exec_params('SELECT movies.id AS id, movies.title AS movie, movies.year AS year, actors.name AS actor,
       genres.name AS genre, studios.name AS studio, cast_members.movie_id AS cast_movie_id, cast_members.character AS role,
       cast_members.actor_id AS actor_id, actors.id AS actor_id_f
       FROM movies
@@ -65,20 +65,9 @@ get '/movies/:id' do
       JOIN studios ON movies.studio_id = studios.id
       JOIN cast_members ON movies.id = cast_members.movie_id
       JOIN actors on actors.id = cast_members.actor_id
-      ORDER BY movie')
+      WHERE movies.id = $1',[@movie_id])
     @movies_id = results.to_a
   end
-  movies_hash = {}
-  @movies_id.each do |movie|
-    movies_hash[movie["id"]] = [movie["movie"], movie["genre"], movie["studio"], movie["actor"], movie["role"], movie["actor_id"]]
-  end
-  @movies_hash = movies_hash
-  @movie_movie = movies_hash[@movie_id][0]
-  @genre_movie = movies_hash[@movie_id][1]
-  @studio_movie = movies_hash[@movie_id][2]
-  @actor_movie = movies_hash[@movie_id][3]
-  @role_movie = movies_hash[@movie_id][4]
-  @actor_id_movie = movies_hash[@movie_id][5]
 
   erb :'movies/show'
 end
