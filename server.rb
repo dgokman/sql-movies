eefa1bf0edd71ee0bed38d6e1c514299403e674b
Dennis Gokman <dennis.gokman@gmail.com>
2014-08-26 17:03:14 -0400

    added actor links

--- server.rb
+++ server.rb
@@ -24,6 +24,22 @@ get '/actors' do
   #binding.pry
 end
 
+get '/actors/:id' do
+  @actor_id = params[:id]
+  db_connection do |conn|
+    results = conn.exec_params('SELECT movies.title AS movie, movies.id AS movie_id,
+      cast_members.character AS role,cast_members.actor_id AS actor_id,
+      actors.id AS id, cast_members.movie_id AS cast_movie_id, actors.name AS actor
+      FROM cast_members
+      JOIN movies ON movies.id = cast_members.movie_id
+      JOIN actors ON actors.id = cast_members.actor_id
+      WHERE actors.id = $1', [@actor_id])
+    @actors_id = results.to_a
+    #binding.pry
+  end
+  erb :'actors/show'
+end
+
 get '/movies' do
   db_connection do |conn|
     results = conn.exec('SELECT movies.id AS id, movies.title AS movie, movies.year AS year, movies.rating AS rating,
@@ -36,6 +52,35 @@ get '/movies' do
     #binding.pry
   end
   erb :'movies/index'
+end
+
+get '/movies/:id' do
+  @movie_id = params[:id]
+  db_connection do |conn|
+    results = conn.exec('SELECT movies.id AS id, movies.title AS movie, movies.year AS year, actors.name AS actor,
+      genres.name AS genre, studios.name AS studio, cast_members.movie_id AS cast_movie_id, cast_members.character AS role,
+      cast_members.actor_id AS actor_id, actors.id AS actor_id_f
+      FROM movies
+      JOIN genres ON movies.genre_id = genres.id
+      JOIN studios ON movies.studio_id = studios.id
+      JOIN cast_members ON movies.id = cast_members.movie_id
+      JOIN actors on actors.id = cast_members.actor_id
+      ORDER BY movie')
+    @movies_id = results.to_a
+  end
+  movies_hash = {}
+  @movies_id.each do |movie|
+    movies_hash[movie["id"]] = [movie["movie"], movie["genre"], movie["studio"], movie["actor"], movie["role"], movie["actor_id"]]
+  end
+  @movies_hash = movies_hash
+  @movie_movie = movies_hash[@movie_id][0]
+  @genre_movie = movies_hash[@movie_id][1]
+  @studio_movie = movies_hash[@movie_id][2]
+  @actor_movie = movies_hash[@movie_id][3]
+  @role_movie = movies_hash[@movie_id][4]
+  @actor_id_movie = movies_hash[@movie_id][5]
+
+  erb :'movies/show'
 end
 
 
